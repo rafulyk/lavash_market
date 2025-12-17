1c2cce1dd4c8e69ed7289a13d8ea826c304c45f3
rafulyk <rafulyk@gmail.com>
2025-12-17 16:10:39 +0300

    Added accessibility and updated navbar icons. Fixed bugs.

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-const infoPage = document.getElementById('info-page');
-const orderPage = document.getElementById('order-page');
-const accountPage = document.getElementById('account-page');
-
-
+document.addEventListener('touchstart', function() {}, {passive: true});
 const btns = document.querySelectorAll('.nav-btn');
 const pages = document.querySelectorAll('.page');
 const selected = document.getElementById('selected');
-const selectedCircle = ['translateX(calc(-50% - 100px))', 'translateX(-50%)', 'translateX(calc(-50% + 100px))'];
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < pages.length; i++) {
     btns[i].addEventListener('click', () => {
-        selected.style.transform = selectedCircle[i];
-        pages.forEach(item => item.style.display = 'none');
-        pages[i].style.display = 'block';
+        selected.style.transform = `translateX(calc(-50% - 100px + ${i} * 100px))`;
+        pages.forEach(item => item.setAttribute('hidden', ''));
+        btns.forEach(item => item.setAttribute('aria-selected', 'false'));
+        pages[i].removeAttribute('hidden');
+        btns[i].setAttribute('aria-selected', 'true');
     })
 }

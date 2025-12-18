fe6cb1052832478938178ac70862d6253acd64fb
rafulyk <rafulyk@gmail.com>
2025-12-18 20:37:24 +0300

    Added JS-powered catalog-cart for ordering

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ for (let i = 0; i < pages.length; i++) {
         btns[i].setAttribute('aria-selected', 'true');
     })
 }
+const productBoxes = document.querySelectorAll('.product-box');
+const totalQuantity = document.getElementById('total-quantity');
+const orderList = document.querySelectorAll('.order-item');
+
+let order = [0, 0, 0, 0, 0, 0, 0];
+for (let i = 0; i < productBoxes.length; i++) {
+    const quantityBtns = productBoxes[i].querySelectorAll('.q-btn');
+    const quantityValue = productBoxes[i].querySelector('.quantity-value');
+    const productName = productBoxes[i].querySelector('.product-name');
+    const productInfo = productBoxes[i].querySelector('.product-info');
+    let quantity = 0;
+    for (let char of quantityBtns) {
+        const btnValue = char.querySelector('.btn-value');
+        char.addEventListener('click', () => {
+            quantity += Number(btnValue.textContent);
+            if (quantity < 0) {
+                quantity = 0;
+            } else {
+                orderList[i].textContent = `${productName.textContent} ${productInfo.textContent} - ${quantity}шт`;
+            }
+            order[i] = quantity;
+            quantityValue.textContent = quantity;
+            let total = 0;
+            for (let quan of order) {
+                total += quan;
+            }
+            totalQuantity.textContent = total;
+        })     
+    }
+}
